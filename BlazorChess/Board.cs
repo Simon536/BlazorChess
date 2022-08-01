071281ce8abcc38f20e0060b4d0c9230c12566fc
SimonG <simon.grunwald@outlook.com>
2022-08-01 21:13:18 +1000

    Started bitboard implementation. No speed boost yet

--- BlazorChess/Board.cs
+++ BlazorChess/Board.cs
@@ -7,6 +7,15 @@ namespace ChessEngine
 
         internal int Score;
 
+        internal ulong pawns;
+        internal ulong rooks;
+        internal ulong knights;
+        internal ulong bishops;
+        internal ulong queens;
+        internal ulong kings;
+        internal ulong white;
+        internal ulong occupied;
+
         internal bool BlackInCheck;
         internal bool BlackCheckMated;
         internal bool WhiteInCheck;
@@ -54,6 +63,15 @@ namespace ChessEngine
         internal Board FastCopy()
         {
             Board clonedBoard = new Board(Squares);
+
+            clonedBoard.pawns = pawns;
+            clonedBoard.rooks = rooks;
+            clonedBoard.knights = knights;
+            clonedBoard.bishops = bishops;
+            clonedBoard.queens = queens;
+            clonedBoard.kings = kings;
+            clonedBoard.occupied = occupied;
+            clonedBoard.white = white;
 
             clonedBoard.EndGamePhase = EndGamePhase;
             clonedBoard.WhosMove = WhosMove;
@@ -194,17 +212,7 @@ namespace ChessEngine
             EndGamePhase = false;
 
             //  Note: There are 32 pieces in the beginning
-            int pieceCount = 0;
-
-            for (byte i = 0; i < 64; i++)
-            {
-                if (Squares[i].piece != null)
-                {
-                    pieceCount++;
-                }
-            }
-
-            if (pieceCount < 10)
+            if (utils.popCount(occupied) < 10)
             {
                 EndGamePhase = true;
             }
@@ -319,6 +327,79 @@ namespace ChessEngine
             
         }
 
+        internal void updateOccupied()
+        {
+            occupied = pawns | rooks | knights | bishops | queens | kings;
+        }
+
+        internal string GetPieceCode(int squareNum)
+        {
+            ulong mask = 1ul << squareNum;
+
+            // Check if square is empty
+            if ((occupied & mask) == 0)
+            {
+                return "\u3164";
+            }
+
+            // Check for white pieces
+            if ((white & mask) != 0)
+            {
+                if ((pawns & mask) != 0)
+                {
+                    return "\u2659";
+                }
+                else if ((rooks & mask) != 0)
+                {
+                    return "\u2656";
+                }
+                else if ((knights & mask) != 0)
+                {
+                    return "\u2658";
+                }
+                else if ((bishops & mask) != 0)
+                {
+                    return "\u2657";
+                }
+                else if ((queens & mask) != 0)
+                {
+                    return "\u2655";
+                }
+                else if ((kings & mask) != 0)
+                {
+                    return "\u2654";
+                }
+                else return "";
+            }
+            else // It's a black piece
+            {
+                if ((pawns & mask) != 0)
+                {
+                    return "\u265f";
+                }
+                else if ((rooks & mask) != 0)
+                {
+                    return "\u265c";
+                }
+                else if ((knights & mask) != 0)
+                {
+                    return "\u265e";
+                }
+                else if ((bishops & mask) != 0)
+                {
+                    return "\u265d";
+                }
+                else if ((queens & mask) != 0)
+                {
+                    return "\u265b";
+                }
+                else if ((kings & mask) != 0)
+                {
+                    return "\u265a";
+                }
+                else return "";
+            }
+        }
 
         /// <summary>
         /// Now using Unicode :)
@@ -406,10 +487,6 @@ namespace ChessEngine
             }
         }
 
-        /// <summary>
-        /// Zob hash values are currently hardcoded here. Bear that in mind.
-        /// </summary>
-        /// <returns></returns>
         public static Board setStartingPosition()
         {
             Board b = new();
@@ -419,38 +496,68 @@ namespace ChessEngine
             for (byte i = 8; i < 16; i++)
             {
                 b.Squares[i].piece = new Piece(ChessPieceType.Pawn, ChessPieceColour.Black);
+                b.pawns = utils.setBit(b.pawns, i);
             }
 
             for (byte i = 48; i < 56; i++)
             {
                 b.Squares[i].piece = new Piece(ChessPieceType.Pawn, ChessPieceColour.White);
+                b.pawns = utils.setBit(b.pawns, i);
+                b.white = utils.setBit(b.white, i);
             }
 
             //  Add Rooks
             b.Squares[0].piece = new Piece(ChessPieceType.Rook, ChessPieceColour.Black);
             b.Squares[7].piece = new Piece(ChessPieceType.Rook, ChessPieceColour.Black);
+            b.rooks = utils.setBit(b.rooks, 0);
+            b.rooks = utils.setBit(b.rooks, 7);
             b.Squares[56].piece = new Piece(ChessPieceType.Rook, ChessPieceColour.White);
             b.Squares[63].piece = new Piece(ChessPieceType.Rook, ChessPieceColour.White);
+            b.rooks = utils.setBit(b.rooks, 56);
+            b.rooks = utils.setBit(b.rooks, 63);
+            b.white = utils.setBit(b.white, 56);
+            b.white = utils.setBit(b.white, 63);
 
             //  Add Knights
             b.Squares[1].piece = new Piece(ChessPieceType.Knight, ChessPieceColour.Black);
             b.Squares[6].piece = new Piece(ChessPieceType.Knight, ChessPieceColour.Black);
+            b.knights = utils.setBit(b.knights, 1);
+            b.knights = utils.setBit(b.knights, 6);
             b.Squares[57].piece = new Piece(ChessPieceType.Knight, ChessPieceColour.White);
             b.Squares[62].piece = new Piece(ChessPieceType.Knight, ChessPieceColour.White);
+            b.knights = utils.setBit(b.knights, 57);
+            b.knights = utils.setBit(b.knights, 62);
+            b.white = utils.setBit(b.white, 57);
+            b.white = utils.setBit(b.white, 62);
 
             //  Add Bishops
             b.Squares[2].piece = new Piece(ChessPieceType.Bishop, ChessPieceColour.Black);
             b.Squares[5].piece = new Piece(ChessPieceType.Bishop, ChessPieceColour.Black);
+            b.bishops = utils.setBit(b.bishops, 2);
+            b.bishops = utils.setBit(b.bishops, 5);
             b.Squares[58].piece = new Piece(ChessPieceType.Bishop, ChessPieceColour.White);
             b.Squares[61].piece = new Piece(ChessPieceType.Bishop, ChessPieceColour.White);
+            b.bishops = utils.setBit(b.bishops, 58);
+            b.bishops = utils.setBit(b.bishops, 61);
+            b.white = utils.setBit(b.white, 58);
+            b.white = utils.setBit(b.white, 61);
 
             //  Add Queens
             b.Squares[3].piece = new Piece(ChessPieceType.Queen, ChessPieceColour.Black);
+            b.queens = utils.setBit(b.queens, 3);
             b.Squares[59].piece = new Piece(ChessPieceType.Queen, ChessPieceColour.White);
+            b.queens = utils.setBit(b.queens, 59);
+            b.white = utils.setBit(b.white, 59);
 
             //  Add Kings
             b.Squares[4].piece = new Piece(ChessPieceType.King, ChessPieceColour.Black);
+            b.kings = utils.setBit(b.kings, 4);
             b.Squares[60].piece = new Piece(ChessPieceType.King, ChessPieceColour.White);
+            b.kings = utils.setBit(b.kings, 60);
+            b.white = utils.setBit(b.white, 60);
+
+
+            b.occupied = b.pawns | b.rooks | b.knights | b.bishops | b.queens | b.kings;
 
             return b;
         }
--- BlazorChess/MoveHandler.cs
+++ BlazorChess/MoveHandler.cs
@@ -1,16 +1,36 @@
 ﻿namespace ChessEngine
 {
-    public struct moveStruct
+    public enum directions
     {
-        public ulong hash;
-        public int score;
-        public byte moveOrigin;
-        public byte moveDestination;
-        public sbyte depth;
+        North,
+        South,
+        West,
+        East,
+        NorthWest,
+        NorthEast,
+        SouthWest,
+        SouthEast
+    }
+
+    public enum nameToIndex
+    {
+        A8, B8, C8, D8, E8, F8, G8, H8,
+        A7, B7, C7, D7, E7, F7, G7, H7,
+        A6, B6, C6, D6, E6, F6, G6, H6,
+        A5, B5, C5, D5, E5, F5, G5, H5,
+        A4, B4, C4, D4, E4, F4, G4, H4,
+        A3, B3, C3, D3, E3, F3, G3, H3,
+        A2, B2, C2, D2, E2, F2, G2, H2,
+        A1, B1, C1, D1, E1, F1, G1, H1
     }
 
     internal static class MoveHandler
     {
+        static ulong[,] rayAttacks = new ulong[8,64];
+
+        private const ulong notA = 0xFEFEFEFEFEFEFEFE;
+        private const ulong notH = 0x7F7F7F7F7F7F7F7F;
+
         /// <summary>
         /// Please note: this is also used by the AI
         /// </summary>
@@ -26,6 +46,52 @@
                 Piece movingPiece = board.Squares[startingPos].piece;
                 board.Squares[startingPos].piece = null;
                 board.Squares[endingPos].piece = movingPiece;
+
+                board.pawns = utils.clearBit(board.pawns, endingPos);
+                board.rooks = utils.clearBit(board.rooks, endingPos);
+                board.knights = utils.clearBit(board.knights, endingPos);
+                board.bishops = utils.clearBit(board.bishops, endingPos);
+                board.queens = utils.clearBit(board.queens, endingPos);
+                board.kings = utils.clearBit(board.kings, endingPos);
+
+                switch (movingPiece.pieceType)
+                {
+                    case ChessPieceType.Pawn:
+                        board.pawns = utils.setBit(board.pawns, endingPos);
+                        board.pawns = utils.clearBit(board.pawns, startingPos);
+                        break;
+                    case ChessPieceType.Rook:
+                        board.rooks = utils.setBit(board.rooks, endingPos);
+                        board.rooks = utils.clearBit(board.rooks, startingPos);
+                        break;
+                    case ChessPieceType.Knight:
+                        board.knights = utils.setBit(board.knights, endingPos);
+                        board.knights = utils.clearBit(board.knights, startingPos);
+                        break;
+                    case ChessPieceType.Bishop:
+                        board.bishops = utils.setBit(board.bishops, endingPos);
+                        board.bishops = utils.clearBit(board.bishops, startingPos);
+                        break;
+                    case ChessPieceType.Queen:
+                        board.queens = utils.setBit(board.queens, endingPos);
+                        board.queens = utils.clearBit(board.queens, startingPos);
+                        break;
+                    case ChessPieceType.King:
+                        board.kings = utils.setBit(board.kings, endingPos);
+                        board.kings = utils.clearBit(board.kings, startingPos);
+                        break;
+                    default:
+                        break;
+                }
+
+                if (movingPiece.pieceColour == ChessPieceColour.White)
+                {
+                    board.white = utils.setBit(board.white, endingPos);
+                }
+                else
+                {
+                    board.white = utils.clearBit(board.white, endingPos);
+                }
 
                 //  Handle promotions (White)
                 if (endingPos < 8 && movingPiece.pieceType == ChessPieceType.Pawn)
@@ -49,6 +115,8 @@
                 {
                     board.BlackKingHasMoved = true;
                 }
+
+                board.updateOccupied();
 
                 return true;
             }
@@ -104,6 +172,109 @@
             }
 
             return false;
+        }
+
+        public static ulong getRayAttacks(ulong occupied, directions dir, int square)
+        {
+            ulong attacks = rayAttacks[(int)dir, square];
+            ulong blockers = attacks & occupied;
+            int block = utils.bitScanForward(blockers | 0x8000000000000000); // Find location of first blocking piece
+            return attacks ^ rayAttacks[(int)dir, block];
+        }
+
+        public static void initRayAttacks()
+        {
+            for (int sq = 0; sq < 64; sq++)
+            {
+                // North
+                ulong attackmask = 0UL;
+                for (int bit = sq - 8; bit >= 0; bit -= 8)
+                {
+                    attackmask |= (1ul << bit);
+                }
+                rayAttacks[(int)directions.North, sq] = attackmask;
+
+                // South
+                attackmask = 0UL;
+                for (int bit = sq + 8; bit <= 63; bit += 8)
+                {
+                    attackmask |= (1ul << bit);
+                }
+                rayAttacks[(int)directions.South, sq] = attackmask;
+
+                // East
+                attackmask = 0UL;
+                for (int bit = sq + 1; bit <= 63; bit++)
+                {
+                    if (((1ul << bit) & notA) != 0)
+                    {
+                        attackmask |= (1ul << bit);
+                    }
+                    else break;
+                }
+                rayAttacks[(int)directions.East, sq] = attackmask;
+
+                // West
+                attackmask = 0UL;
+                for (int bit = sq - 1; bit >= 0; bit--)
+                {
+                    if (((1ul << bit) & notH) != 0)
+                    {
+                        attackmask |= (1ul << bit);
+                    }
+                    else break;
+                }
+                rayAttacks[(int)directions.West, sq] = attackmask;
+
+                // NorthEast
+                attackmask = 0UL;
+                for (int bit = sq - 7; bit >= 0; bit -= 7)
+                {
+                    if (((1ul << bit) & notA) != 0)
+                    {
+                        attackmask |= (1ul << bit);
+                    }
+                    else break;
+                }
+                rayAttacks[(int)directions.NorthEast, sq] = attackmask;
+
+                // NorthWest
+                attackmask = 0UL;
+                for (int bit = sq - 9; bit >= 0; bit -= 9)
+                {
+                    if (((1ul << bit) & notH) != 0)
+                    {
+                        attackmask |= (1ul << bit);
+                    }
+                    else break;
+                }
+                rayAttacks[(int)directions.NorthWest, sq] = attackmask;
+
+                // SouthEast
+                attackmask = 0UL;
+                for (int bit = sq + 9; bit <= 63; bit += 9)
+                {
+                    if (((1ul << bit) & notA) != 0)
+                    {
+                        attackmask |= (1ul << bit);
+                    }
+                    else break;
+                }
+                rayAttacks[(int)directions.SouthEast, sq] = attackmask;
+
+                // SouthWest
+                attackmask = 0UL;
+                for (int bit = sq + 7; bit <= 63; bit += 7)
+                {
+                    if (((1ul << bit) & notH) != 0)
+                    {
+                        attackmask |= (1ul << bit);
+                    }
+                    else break;
+                }
+                rayAttacks[(int)directions.SouthWest, sq] = attackmask;
+            }
+
         }
     }
 }
